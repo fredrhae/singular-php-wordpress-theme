ace76909973a0cbb447c5bf9fabcb5d5af407489
Fred Rhae <fredrhae@gmail.com>
2017-10-29 22:39:12 -0200

    Code for display each solution individually

--- single.php
+++ single.php
@@ -3,4 +3,29 @@
     require_once('header.php');
 ?>
 
-<?php get_footer?>
+<?php 
+    if( have_posts() ) 
+    {
+        while( have_posts() ) 
+        {
+            the_post(); ?>
+            <div class="container-principal pb-5">
+                <div class="single-solucao-thumbnail">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+                <div class="container">
+                    <section class="chamada-principal">
+                        <h1><?php the_title(); ?></h1>
+                    </section>
+                    <section class="single-solucao-geral">
+                        <div class="single-solucao-descricao">
+                            <?php the_content(); ?>
+                        </div>
+                    </section>
+                </div>
+            </div>
+    <?php 
+        }
+    } 
+    ?>
+<?php get_footer()?>
