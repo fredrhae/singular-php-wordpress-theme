41133200c5a2ba09aa2ec4a559474f1ab5685b90
Fred Rhae <fredrhae@gmail.com>
2017-10-29 23:31:38 -0200

    Fixing for page solution be responsive

--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 <?php 	
-    $css_escolhido = 'page';
+    $css_escolhido = 'single';
     require_once('header.php');
 ?>
 
@@ -8,20 +8,24 @@
     {
         while( have_posts() ) 
         {
-            the_post(); ?>
+            the_post(); 
+            $imagem_destacada = wp_get_attachment_url( get_post_thumbnail_id($post->ID));                
+            ?>
             <div class="container-principal pb-5">
-                <div class="single-solucao-thumbnail">
-                    <?php the_post_thumbnail(); ?>
+                <div class="wrapper col-md-12 px-0 mb-3">
+                    <img class="img-responsive" src="<?=$imagem_destacada?>" >
                 </div>
                 <div class="container">
-                    <section class="chamada-principal">
-                        <h1><?php the_title(); ?></h1>
-                    </section>
-                    <section class="single-solucao-geral">
-                        <div class="single-solucao-descricao">
-                            <?php the_content(); ?>
-                        </div>
-                    </section>
+                    <div class="col-md-12">
+                        <section class="chamada-principal">
+                            <h1><?php the_title(); ?></h1>
+                        </section>
+                        <section class="single-solucao-geral">
+                            <div class="single-solucao-descricao">
+                                <?php the_content(); ?>
+                            </div>
+                        </section>
+                    </div>
                 </div>
             </div>
     <?php 
