7be79cac76ee61b0f1573bc60e3e5b240846b7bf
Fred Rhae <fredrhae@gmail.com>
2017-11-16 23:09:34 -0200

    Finalized the addition of projects using custom post
    The projects are supporting attachment of many images and are
    loading from the project configured on wordpress.

--- page.php
+++ page.php
@@ -415,6 +415,66 @@ function get_solucoes_body_content() { ?>
 <!-- #########   Funcoes relacionadas a pagina projetos ############### -->
 <?php 
 function get_projetos_body_content() { ?>
-	
-<?php 
+	<!-- Carregamento das soluções cadastradas -->
+	<?php 
+	$args = array( 'post_type' => 'projeto', 'order' => 'ASC' );            
+	$loop = new WP_Query( $args );
+	if( $loop->have_posts() ) { 
+		$counter = 0;
+		?>
+	<?php while( $loop->have_posts()) {
+		// Flag para indicar se a solucao vai ser exibida na direita ou a esquerda
+		$alignRight = $counter%2 != 0;		
+		$loop->the_post(); ?>
+		<div class="row w-100 m-0 py-2 px-0">
+			<?php if($alignRight) { ?>
+				<div class="col-md-6 pr-2 pb-1 px-0">
+					<?php
+					$attachments = new Attachments( 'my_attachments' );
+					if( $attachments->exist() ) : ?>
+						<?php while( $attachments->get() ) : ?>
+							<img class="img-fluid py-1" src="<?=$attachments->url()?>" alt="">
+						<?php endwhile; ?>
+					<?php endif; ?>			
+				</div>
+				<div class="col-md-6 pl-0 py-1 pr-0">
+					<div class="d-flex flex-column align-items-center full-image-background h-100" style="background-image: url(<?=the_post_thumbnail_url()?>)">
+						<div class="my-auto pt-5">
+							<h2 class="titulo-solucao"><?php the_title();?></h2>
+						</div>
+						<div class="w-50 texto-solucao block-with-text my-auto">
+							<?php the_content();?>
+						</div>
+					</div>
+				</div>
+			<?php 
+			} else {
+			?>
+				<div class="col-md-6 pr-2 py-1 px-0">
+					<div class="d-flex flex-column align-items-center full-image-background h-100" style="background-image: url(<?=the_post_thumbnail_url()?>)">
+						<div class="my-auto pt-5">
+							<h2 class="titulo-solucao"><?php the_title();?></h2>
+						</div>
+						<div class="w-50 texto-solucao block-with-text my-auto">
+							<?php the_content();?>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 pl-0 pr-0">
+					<?php
+					$attachments = new Attachments( 'my_attachments' );
+					if( $attachments->exist() ) : ?>
+						<?php while( $attachments->get() ) : ?>
+							<img class="img-fluid py-1" src="<?=$attachments->url()?>" alt="">
+						<?php endwhile; ?>
+					<?php endif; ?>			
+				</div>
+			<?php 
+			}
+			?>
+		</div>
+		<?php
+		$counter = $counter + 1;
+		} 
+	} 
 }?>
